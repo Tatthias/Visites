2671858aba5390ce7184a08dc9728df610c1a610
Mewen-Lucas <lucas.mewen1@gmail.com>
2021-05-27 14:08:46 +0200

    liste des visites

--- app/src/main/java/com/example/visitesapprentis/IHM/MainActivity.java
+++ app/src/main/java/com/example/visitesapprentis/IHM/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.visitesapprentis.IHM;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -20,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Button bAjouter;
     ListView listView;
+    private ApprentiDAO apprentiDAO;
+    private int idApp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,25 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listeApprenti);
 
+        apprentiDAO = new ApprentiDAO(getApplicationContext());
+        apprentiDAO.open();
+
+
+        for (Apprenti unApp : apprentiDAO.read()) {
+            idApp = unApp.getIdApp() + 1;
+        }
+        apprentiDAO.close();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Dessert dessert = desserts.get(i);
+                for ( i = 0; i < idApp-1; i++) {
+                    Intent intent = new Intent(MainActivity.this, VisitesActivity.class);
+                    startActivityForResult(intent, 0);
+                }
+            }
+        });
         List<Apprenti> lesApprentis = new ArrayList<>();
         ApprentiDAO apprentiDAO = new ApprentiDAO(getApplicationContext());
         apprentiDAO.open();
---
+++ app/src/main/java/com/example/visitesapprentis/IHM/VisitesActivity.java
@@ -0,0 +1,16 @@
+package com.example.visitesapprentis.IHM;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.visitesapprentis.R;
+
+public class VisitesActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visites);
+
+
+    }
+}
